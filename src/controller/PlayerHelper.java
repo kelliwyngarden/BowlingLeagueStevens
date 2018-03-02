af830ca8dea1aad2cd6992879eec976d8ca0397f
dmpettigrew <dmpettigrew@users.noreply.github.com>
2018-03-02 14:22:09 -0600

    Adding Team-Add-View-Delete

--- src/controller/PlayerHelper.java
+++ src/controller/PlayerHelper.java
@@ -1,23 +1,48 @@
 package controller;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import model.Player;
 
-
 public class PlayerHelper {
-EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("BowlingLeagueStevens"); 
-	
+	EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("bowlingleaguestevens");
 
+	public void addPlayer(Player toAdd) {
+		// TODO Auto-generated method stub
 
-	public void insertPlayer(Player tm) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(toAdd);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public List<Player> showAllPlayers() {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		TypedQuery<Player> allResults = em.createQuery("select p from Player p", Player.class);
+		List<Player> allPlayers = allResults.getResultList();
+		em.close();
+		return allPlayers;
+	}
+
+	public void deletePlayers(Player toDelete) {
 		// TODO Auto-generated method stub
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
-		em.persist(tm);
+		TypedQuery<Player> typedQuery = em.createQuery("select p from Player p where p.firstName = :selectedFirstName and p.lastName = :selectedLastName",Player.class);
+		typedQuery.setParameter("selectedFirstName", toDelete.getFirstName());
+		typedQuery.setParameter("selectedLastName", toDelete.getLastName());
+		typedQuery.setMaxResults(1);
+		Player result = typedQuery.getSingleResult();
+		em.remove(result);
 		em.getTransaction().commit();
 		em.close();
-}
+	
 	}
+}
